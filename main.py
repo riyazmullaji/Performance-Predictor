2dd83bfdb4aa65fe025d831aa07b48f3ea08c690
riyaz <riyazmullaji02@gmail.com>
2024-09-09 00:31:11 +0530

    Data preprocessing

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ from src.mlproject.exception import CustomException
 import sys
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig
-
+from src.mlproject.components.data_transformation import DataTransformation
 
 if __name__ == '__main__':
     logging.info('Running')
     try:
-        data_ingestion=DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        
+       data_ingestion=DataIngestion()
+       train_path,test_path = data_ingestion.initiate_data_ingestion()
+       data_transformation = DataTransformation()
+       data_transformation.initiate_data_transformation(train_path,test_path)
+       
+    
     except Exception as e:
         logging.info('Exception')
         raise CustomException(e,sys) 
--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -7,8 +7,20 @@ from dotenv import load_dotenv
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import r2_score
 import pymysql
-
+import pickle
 import numpy as np
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
 
 load_dotenv()
 
